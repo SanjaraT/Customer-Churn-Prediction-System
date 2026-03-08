59ae4a06bc1e8576f980c0b5ccfad6d5b06ce572
sanjaraT <sanjaratunola@gmail.com>
2026-03-08 12:24:07 +0600

    Backend Connection

--- frontend/config.py
+++ frontend/config.py
@@ -1,3 +1,3 @@
-API_BASE_URL = "http://127.0.0.1:8000"
+API_BASE_URL = "https://customer-churn-prediction-system-backend.onrender.com"
 PREDICT_ENDPOINT = f"{API_BASE_URL}/predict"
 HEALTH_ENDPOINTT = f"{API_BASE_URL}/health"
